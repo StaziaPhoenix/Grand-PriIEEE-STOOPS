8787117dd986b1e90f4ce2bc49cb31898b3fa6d7
Stazia Tronboll <stazia@stazias-mbp.dynamic.ucsd.edu>
2016-02-26 19:50:04 -0800

    Add my working but basic i2c shtuffs.

---
+++ Code/servotest.c
@@ -0,0 +1,60 @@
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+ 
+// The PiWeather board i2c address
+#define ADDRESS 0x04
+
+int file;
+unsigned char values[2] = {0,1};
+ 
+// The I2C bus: This is for V2 pi's. For V1 Model B you need i2c-0
+static const char *devName = "/dev/i2c-1";
+ 
+int main(int argc, char** argv) {
+ 
+  printf("I2C: Connecting\n");
+
+ 
+  if ((file = open(devName, O_RDWR)) < 0) {
+    fprintf(stderr, "I2C: Failed to access %d\n", devName);
+    return 1;
+  }
+ 
+  printf("I2C: acquiring buss to 0x%x\n", ADDRESS);
+ 
+  if (ioctl(file, I2C_SLAVE, ADDRESS) < 0) {
+    fprintf(stderr, 
+	"I2C: Failed to acquire bus access/talk to slave 0x%x\n", ADDRESS);
+    exit(1);
+  }
+ 
+  int i;
+  for (i = 0; i < 180; i = i+10) {
+    values[0] = i;
+    values[1] = values[1]*2 % 250;
+
+    /*if (i % 20 == 0) {
+      values[1] = 64;
+    } else {
+      values[1] = 255;
+    }*/
+
+    printf("Sending %d, %d\n", values[0], values[1]);
+    write(file, values, 1);
+    usleep(1000000);
+  }
+ 
+
+  // Now wait else you could crash the arduino by sending requests too fast
+  usleep(10000);
+ 
+  close(file);
+  return 0;
+}
