fe2f51ed54c58fa46a38edf16ae0f0bd44f612ee
Painguy <nganjehl@ucsd.edu>
2016-03-18 13:11:20 -0700

    Can manipulate servo from file. Now writing control.

--- Code/newServo.cpp
+++ Code/newServo.cpp
@@ -3,8 +3,8 @@
 int main () {
   FILE * file;
   file = fopen ("/dev/servoblaster","w+");
-  fprintf(file, "0=50%%");
-  fprintf(stdout,"0=50%%");
+  fprintf(file, "0=70%\n");
+  fprintf(stdout,"0=70%\n");
   fclose (file);
 
   return 0;
